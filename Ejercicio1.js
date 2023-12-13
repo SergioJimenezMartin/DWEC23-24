0d2c24217dadc45a6a797814c4a458a6bb5198cc
SergioJimenezMartin <jimenez.sergio@educa.madrid.org>
2023-12-13 09:33:33 +0100

    actualizado

--- Ejercicio1.js
+++
@@ -1,7 +0,0 @@
-let nombre = prompt("¿Cuál es tu nombre?");
-console.log("Hola, " + nombre + "!");
-console.log(`Hola, ${nombre}!`);
-console.log(typeof nombre);
-console.log(nombre.length);
-let esAlumno = prompt("¿Eres alumno de nuestra clase?");
-alert("¿Eres alumno de nuestra clase? " + (esAlumno === "sí" ? "Sí, eres alumno de nuestra clase." : "No, no eres alumno de nuestra clase."));
--- ejerciciosets.js
+++
@@ -1,7 +0,0 @@
-//Unir tres arrays en uno eliminando los elemntos duplicados
-let miArray1 = [1, 2, 3, 4, 5, 6, 7, 8]
-let miArray2 = [1, 2,3, 8, 9, 0, 43, 45, 65]
-let miArray3 = [0, 17, 32, 87, 45]
-
-let miSet = new Set ([miArray1].concat[miArray2].concat[miArray3]);
-let miArrayFinal = [...miSet]
